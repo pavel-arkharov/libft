fac959d758acaf130ad12f4b3ada6c6f7e5d574d
Pavel Arkharov <parkharo@c1r1p11.hive.fi>
2021-12-19 19:27:11 +0200

    fixed lstprint to print content value instead of incorrectly casted memory value

--- ft_lstprint.c
+++ ft_lstprint.c
@@ -6,7 +6,7 @@
 /*   By: parkharo <parkharo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 13:44:02 by parkharo          #+#    #+#             */
-/*   Updated: 2021/11/24 16:23:26 by parkharo         ###   ########.fr       */
+/*   Updated: 2021/12/19 19:26:21 by parkharo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_lstprint(t_list	*lst)
 			ft_putstr("\n\nNode named: ");
 			ft_putstr((char *)lst->content);
 			ft_putstr("\nNext is : ");
-			ft_putnbr((int)lst->next);
+			ft_putstr((char *)lst->next->content);
 			lst = lst->next;
 		}
 		ft_putendl("\nEnd of the list");
