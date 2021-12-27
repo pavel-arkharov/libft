250586f04777bbc92567208ae4c3e717685cbb3e
Pavel Arkharov <parkharo@c2r3p9.hive.fi>
2021-12-27 15:09:03 +0200

    added ft_chrsep function for get_next_line

--- srcs/ft_chrsep.c
+++ srcs/ft_chrsep.c
@@ -1,37 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsep.c                                        :+:      :+:    :+:   */
+/*   ft_chrsep.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: parkharo <parkharo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/26 20:52:38 by parkharo          #+#    #+#             */
-/*   Updated: 2021/12/26 21:46:09 by parkharo         ###   ########.fr       */
+/*   Created: 2021/12/27 14:56:59 by parkharo          #+#    #+#             */
+/*   Updated: 2021/12/27 15:01:46 by parkharo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strsep(char **stringp, const char *delim)
+char	*ft_chrsep(char **stringp, int ch)
 {
 	char	*ptr;
-	size_t	i;
-	//size_t	j;
 
-	i = 0;
-	while (*stringp[i])
+	ptr = ft_strchr(*stringp, ch);
+	if (ptr)
 	{
-		ptr = (char *)delim;
-		while (*ptr != '\0')
-		{
-			if (*ptr == *stringp[i])
-			{
-				*stringp[i++] = '\0';
-				return (&(*stringp[i]));
-			}
-			ptr++;
-		}
-		++i;
+		*ptr = '\0';
+		ptr++;
 	}
-	return (NULL);
+	return (ptr);
 }
